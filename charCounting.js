22ead7f9b660d1ac87f830d0a9c4072af9b4bd42
Craig <craigricetong@gmail.com>
2018-03-14 15:36:16 +0000

    outcomes

---
+++ charCounting.js
@@ -0,0 +1,8 @@
+// returns all of the unique characters (letters) in a string that is passed into the function
+// also returns a count of how many of each letter
+// the function must return an object with the letters as keys and the count as the value
+
+
+function countLetters() {
+
+}
